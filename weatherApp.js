6c812e1c2e444b59eb056ed6b452c0d658d466d6
yashsharma110 <yashsharma5312@gmail.com>
2023-10-10 18:51:44 +0530

    updated weatherApp.js added Not Found part functionality

--- weatherApp.js
+++ weatherApp.js
@@ -1,13 +1,13 @@
 const userTab = document.querySelector("[data-userWeather]");
 const searchTab = document.querySelector("[data-searchWeather]");
 const userContainer = document.querySelector(".weather-container");
-
 const grantAccessContainer = document.querySelector(
   ".grant-location-container"
 );
 const searchForm = document.querySelector("[data-searchForm]");
 const loadingScreen = document.querySelector(".loading-container");
 const userInfoContainer = document.querySelector(".user-info-container");
+const errorNotFound = document.querySelector(".error-not-found");
 
 //initially vairables need????
 
@@ -94,15 +94,22 @@ function renderWeatherInfo(weatherInfo) {
   const windspeed = document.querySelector("[data-windspeed]");
   const humidity = document.querySelector("[data-humidity]");
   const cloudiness = document.querySelector("[data-cloudiness]");
-  //fetch values from weatherINfo object and put it UI elements
-  cityName.innerText = weatherInfo?.name;
-  countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
-  desc.innerText = weatherInfo?.weather?.[0]?.description;
-  weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-  temp.innerText = `${weatherInfo?.main?.temp} °C`;
-  windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
-  humidity.innerText = `${weatherInfo?.main?.humidity} %`;
-  cloudiness.innerText = `${weatherInfo?.clouds?.all} %`;
+  if (weatherInfo.name === undefined) {
+    userInfoContainer.classList.remove("active");
+    errorNotFound.classList.add("active");
+  } else {
+    userInfoContainer.classList.add("active");
+    errorNotFound.classList.remove("active");
+    //fetch values from weatherINfo object and put it UI elements
+    cityName.innerText = weatherInfo?.name;
+    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
+    desc.innerText = weatherInfo?.weather?.[0]?.description;
+    weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
+    temp.innerText = `${weatherInfo?.main?.temp} °C`;
+    windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity} %`;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all} %`;
+  }
 }
 
 function getLocation() {
@@ -139,7 +146,7 @@ async function fetchSearchWeatherInfo(city) {
   loadingScreen.classList.add("active");
   userInfoContainer.classList.remove("active");
   grantAccessContainer.classList.remove("active");
-
+  errorNotFound.classList.remove("active");
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
